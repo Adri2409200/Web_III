1873ec3e3a59dc56cd12af2ff1caf5333e9d2a2f
Adri2409200 <inesadrianasalazaribanez@gmail.com>
2026-09-04 13:36:42 -0400

    Agrego diseño, navbar, estilos y páginas

--- WEB_III/Pages/Index.cshtml.cs
+++ WEB_III/Pages/Index.cshtml.cs
@@ -1,13 +1,40 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WEB_III.Services.Interfaces;
 
 namespace WEB_III.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IPropietarioService _propietarioService;
+        private readonly IMascotaService     _mascotaService;
+        private readonly IVeterinarioService _veterinarioService;
+        private readonly ICitaService        _citaService;
+
+        public int TotalPropietarios { get; private set; }
+        public int TotalMascotas     { get; private set; }
+        public int TotalVeterinarios { get; private set; }
+        public int TotalCitas        { get; private set; }
+        public int CitasPendientes   { get; private set; }
+
+        public IndexModel(
+            IPropietarioService propietarioService,
+            IMascotaService     mascotaService,
+            IVeterinarioService veterinarioService,
+            ICitaService        citaService)
+        {
+            _propietarioService = propietarioService;
+            _mascotaService     = mascotaService;
+            _veterinarioService = veterinarioService;
+            _citaService        = citaService;
+        }
+
         public void OnGet()
         {
-
+            TotalPropietarios = _propietarioService.ObtenerTodos().Count();
+            TotalMascotas     = _mascotaService.ObtenerTodos().Count();
+            TotalVeterinarios = _veterinarioService.ObtenerTodos().Count();
+            TotalCitas        = _citaService.ObtenerTodos().Count();
+            CitasPendientes   = _citaService.ObtenerPorEstado(Models.Enums.EstadoCita.Pendiente).Count();
         }
     }
 }
